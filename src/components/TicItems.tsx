aebca6896716dcba41077761dfee147320f25e04
Lee-jeong-wook <126264218+Lee-jeong-wook@users.noreply.github.com>
2023-12-27 22:54:13 +0900

    클릭하면 턴에 따라 변경

--- src/components/TicItems.tsx
+++ src/components/TicItems.tsx
@@ -1,7 +1,23 @@
-export const TicItems = () => {
-    return(
-        <>
-        
-        </>
-    )
+import { useState } from "react";
+import styled from "styled-components";
+
+interface TicItemsProps {
+  value: number;
+  onClick: () => void;
 }
+
+export const TicItems: React.FC<TicItemsProps> = ({ value, onClick }) => {
+  return (
+    <StyledItem onClick={onClick}>
+      {!value ? "" : value === 1 ? "O" : "X"}
+    </StyledItem>
+  );
+};
+
+const StyledItem = styled.div`
+  font-size: 50px;
+  line-height: 50px;
+  width: 50px;
+  height: 50px;
+  border: 1px solid #000;
+`;
--- src/components/TicScreen.tsx
+++ src/components/TicScreen.tsx
@@ -1,9 +1,36 @@
-import { TicItems } from "./TicItems"
+import React, { useEffect, useState } from "react";
+import { TicItems } from "./TicItems";
 
-export const TicScreen = () => {
-    return(
-        <>
-            <TicItems />
-        </>
-    )
-}
+export const TicScreen: React.FC = () => {
+  const [items, setItems] = useState<number[]>([1, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [turn, setTurn] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTurn(!turn);
+  }, [items]);
+
+  const clickHandler = (idx: number) => {
+    if (turn) {
+      setItems((prevItems) => {
+        const tmpArr = [...prevItems];
+        tmpArr[idx] = 1;
+        return tmpArr;
+      });
+    } else {
+      setItems((prevItems) => {
+        const tmpArr = [...prevItems];
+        tmpArr[idx] = 2;
+        return tmpArr;
+      });
+    }
+  };
+
+  return (
+    <>
+      <span>{turn ? "X" : "O"}차례</span>
+      {items.map((element, idx) => (
+        <TicItems key={idx} value={items[idx]} onClick={() => clickHandler(idx)} />
+      ))}
+    </>
+  );
+};
